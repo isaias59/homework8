a6da15331e9bfc1321d4759ec458b50ebdfee8ab
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-25 17:08:32 -0700

    Create process_numbers_main.cpp

---
+++ src/process_numbers_main.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+
+#include "process_numbers.h"
+
+int main()
+{
+	std::ifstream input{ "rand_numbers.txt" };
+	std::ofstream odds{ "odds.txt" };
+	std::ofstream evens{ "evens.txt" };
+	split_odd_even (input,odds,evens);
+
+}
