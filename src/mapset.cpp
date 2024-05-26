412e3ff0c3aef2ee4937fee69f5576a5838846ce
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-25 17:50:58 -0700

    Update mapset.cpp

--- src/mapset.cpp
+++ src/mapset.cpp
@@ -14,24 +14,27 @@ std::string to_lowercase(const std::string& str) {
 
 std::set<std::string> load_stopwords(std::istream& stopwords) {
     std::set<std::string> stopwords_set;
-    std::string word;
-    while (stopwords >> word) {
-        stopwords_set.insert(to_lowercase(word));
-    }
+    std::ranges::istream_view<std::string> view{stopwords};
+
+    std::ranges::for_each(view, [&](const std::string& w) {
+        stopwords_set.insert(to_lowercase(w));
+    });
+
     return stopwords_set;
-}
+
 
 std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords) {
     std::map<std::string, int> word_counts;
-    std::string word;
-    while (document >> word) {
-        word = to_lowercase(word);
-        if (stopwords.find(word) == stopwords.end()) {
-            ++word_counts[word];
+    std::ranges::istream_view<std::string> view{document};
+
+    std::ranges::for_each(view, [&](std::string w) {
+        w = to_lowercase(w);
+        if (stopwords.find(w) == stopwords.end()) {
+            ++word_counts[w];
         }
-    }
+    });
+
     return word_counts;
-}
 
 void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output) {
     for (const auto& [word, count] : word_counts) {
