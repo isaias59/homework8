2a65ddd1c13e9439805d9334f85f0c97bd84aef0
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-25 20:51:19 -0700

    Update compute_grades_main.cpp

--- src/compute_grades_main.cpp
+++ src/compute_grades_main.cpp
@@ -1,1 +1,21 @@
+#include <fstream>
+#include "compute_grades.hpp"
 
+using namespace std;
+
+int main()
+{
+	cout << "\nHey";
+	ifstream gradebook{ "gradebook.txt" };
+	cout << "\nHey";
+
+	Gradebook b;
+	gradebook >> b;
+
+	b.validate();
+	b.compute_grades();
+	b.sort();
+
+	ofstream course_grades{ "course_grades.txt" };
+	course_grades << b;
+}
