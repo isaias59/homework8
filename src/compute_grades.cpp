0cb8ae6914af180dc9083298e84e3317007c7d3c
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-29 23:57:22 -0700

    Update compute_grades.cpp

--- src/compute_grades.cpp
+++ src/compute_grades.cpp
@@ -20,13 +20,10 @@ void Student::validate() const {
     }
 }
 
-
-
 void Student::compute_quiz_avg() {
     if (!quiz.empty()) {
         quiz_avg = accumulate(quiz.begin(), quiz.end(), 0.0) / quiz.size();
-    }
-    else {
+    } else {
         quiz_avg = 0.0;
     }
 }
@@ -34,8 +31,7 @@ void Student::compute_quiz_avg() {
 void Student::compute_hw_avg() {
     if (!hw.empty()) {
         hw_avg = accumulate(hw.begin(), hw.end(), 0.0) / hw.size();
-    }
-    else {
+    } else {
         hw_avg = 0.0;
     }
 }
@@ -50,23 +46,20 @@ void Student::compute_grade() {
     compute_course_score();
     if (course_score >= 90) {
         course_grade = "A";
-    }
-    else if (course_score >= 80) {
+    } else if (course_score >= 80) {
         course_grade = "B";
-    }
-    else if (course_score >= 70) {
+    } else if (course_score >= 70) {
         course_grade = "C";
-    }
-    else if (course_score >= 60) {
+    } else if (course_score >= 60) {
         course_grade = "D";
-    }
-    else {
+    } else {
         course_grade = "F";
     }
 }
 
 std::strong_ordering Student::operator<=>(const Student& other) const {
-    return last_name <=> other.last_name;
+    if (auto cmp = last_name <=> other.last_name; cmp != 0) return cmp;
+    return first_name <=> other.first_name;
 }
 
 bool Student::operator==(const Student& other) const {
@@ -98,14 +91,13 @@ std::istream& operator>>(std::istream& in, Student& s) {
     return in;
 }
 
-
 std::ostream& operator<<(std::ostream& out, const Student& s) {
-    out << "Name: " << s.first_name << " " << s.last_name << "\n";
-    out << "HW Ave: " << s.hw_avg << "\n";
-    out << "QZ Ave: " << s.quiz_avg << "\n";
-    out << "Final: " << s.final_score << "\n";
-    out << "Total: " << s.course_score << "\n";
-    out << "Grade: " << s.course_grade << "\n";
+    out << "Name: " << s.first_name << " " << s.last_name << '\n';
+    out << "HW Ave: " << s.hw_avg << '\n';
+    out << "QZ Ave: " << s.quiz_avg << '\n';
+    out << "Final: " << s.final_score << '\n';
+    out << "Total: " << s.course_score << '\n';
+    out << "Grade: " << s.course_grade << '\n';
     return out;
 }
 
@@ -135,7 +127,7 @@ std::istream& operator>>(std::istream& in, Gradebook& b) {
 
 std::ostream& operator<<(std::ostream& out, const Gradebook& b) {
     for (const Student& student : b.students) {
-        out << student << "\n";
+        out << student;
     }
     return out;
 }
